6efcc098c80b282fb7992cdfd1990f1f36cfb42a
Muslim Kareem <muslimkareem@icloud.com>
2023-09-21 10:35:10 +0200

    Big Bang!

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,15 @@
+package com.example
+
+import com.example.plugins.*
+import configureSockets
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+    configureSockets()
+}
